1ef063e58ebad1d417285453545acd0e55779deb
Hannes Tydén <hannes@tyden.name>
2012-11-15 13:48:36 +0100

    Use wrapped `gen` for recursion.

--- z-combinator.rb
+++ z-combinator.rb
@@ -1,14 +1,12 @@
 puts ->() {
 
-  error = ->(n) { throw "SHOULD NEVER BE CALLED" }
-
   ->(gen) {
     gen.(gen)
   }.(
     ->(gen) {
-      ->(code) {
-        ->(n) { n.zero? ? 1 : n * gen.(gen).(n - 1) }
-      }.(error)
+      ->(partial) {
+        ->(n) { n.zero? ? 1 : n * partial.(n - 1) }
+      }.(->(v) { gen.(gen).(v) })
     }
   ).(5)
 
