9ff22fd1d50ff961e553090164e71f6d30b21428
Hannes Tydén <hannes@tyden.name>
2012-11-15 13:55:31 +0100

    Pass in the improver.

--- z-combinator.rb
+++ z-combinator.rb
@@ -2,17 +2,19 @@ puts ->() {
 
   error = ->(n) { throw "SHOULD NEVER BE CALLED" }
 
-  ->(code) {
+  ->(improver) {
     ->(gen) {
       gen.(gen)
     }.(
       ->(gen) {
-        ->(partial) {
-          ->(n) { n.zero? ? 1 : n * partial.(n - 1) }
-        }.(->(v) { gen.(gen).(v) })
+        improver.(->(v) { gen.(gen).(v) })
       }
     )
-  }.(error).(5)
+  }.(
+    ->(partial) {
+      ->(n) { n.zero? ? 1 : n * partial.(n - 1) }
+    }
+  ).(5)
 
 }.()
 
