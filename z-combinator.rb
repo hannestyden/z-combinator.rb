6f27bd3e02578a8ff76bcef0eace819c6b269144
Hannes Tydén <hannes@tyden.name>
2012-11-15 13:09:45 +0100

    Factorial for n=0 using improver.

--- z-combinator.rb
+++ z-combinator.rb
@@ -1,8 +1,14 @@
 puts ->() {
 
-  fact = ->(n) { n.zero? ? 1 : n * fact.(n - 1) }
-  fact.(5)
+  error = ->(n) { throw "SHOULD NEVER BE CALLED" }
+
+  fact_improver = ->(partial) {
+    ->(n) { n.zero? ? 1 : n * partial.(n - 1) }
+  }
+
+  f0 = fact_improver.(error)
+  f0.(0)
 
 }.()
 
-# >> 120
+# >> 1
