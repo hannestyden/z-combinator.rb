7c13d1b6f5bb63aae08ebd2de3c3bc366f6bcbd7
Hannes Tydén <hannes@tyden.name>
2012-11-15 13:42:49 +0100

    Binding introduction refactoring.

--- z-combinator.rb
+++ z-combinator.rb
@@ -1,12 +1,14 @@
 puts ->() {
 
+  error = ->(n) { throw "SHOULD NEVER BE CALLED" }
+
   ->(gen) {
     gen.(gen)
   }.(
     ->(gen) {
-      ->() {
+      ->(code) {
         ->(n) { n.zero? ? 1 : n * gen.(gen).(n - 1) }
-      }.()
+      }.(error)
     }
   ).(5)
 
