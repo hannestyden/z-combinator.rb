6efe0b95701068b519b6d11ab1fc86f4d6752e45
Hannes Tydén <hannes@tyden.name>
2012-11-15 13:50:59 +0100

    Tennent's Correspondence Principle refactoring.

--- z-combinator.rb
+++ z-combinator.rb
@@ -1,14 +1,16 @@
 puts ->() {
 
-  ->(gen) {
-    gen.(gen)
-  }.(
+  ->() {
     ->(gen) {
-      ->(partial) {
-        ->(n) { n.zero? ? 1 : n * partial.(n - 1) }
-      }.(->(v) { gen.(gen).(v) })
-    }
-  ).(5)
+      gen.(gen)
+    }.(
+      ->(gen) {
+        ->(partial) {
+          ->(n) { n.zero? ? 1 : n * partial.(n - 1) }
+        }.(->(v) { gen.(gen).(v) })
+      }
+    )
+  }.().(5)
 
 }.()
 
