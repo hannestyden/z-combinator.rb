b0a955ef290fa8973adeea73c06a413e662358aa
Hannes Tydén <hannes@tyden.name>
2012-11-15 13:23:48 +0100

    Factorial for n=1 using bound improver.

--- z-combinator.rb
+++ z-combinator.rb
@@ -2,31 +2,17 @@ puts ->() {
 
   error = ->(n) { throw "SHOULD NEVER BE CALLED" }
 
-  fact_improver = ->(partial) {
-    ->(n) { n.zero? ? 1 : n * partial.(n - 1) }
-  }
-
   fx =
-    fact_improver.(
-      fact_improver.(
-        fact_improver.(
-          fact_improver.(
-            fact_improver.(
-              fact_improver.(
-                fact_improver.(
-                  fact_improver.(
-                    fact_improver.(
-                      fact_improver.(
-                        fact_improver.(
-                          fact_improver.(
-                            fact_improver.(
-                              fact_improver.(
-                                fact_improver.(
-                                  error
-    ) ) ) ) ) ) ) ) ) ) ) ) ) ) )
+    ->(improver) {
+      improver.(improver.(error))
+    }.(
+      ->(partial) {
+        ->(n) { n.zero? ? 1 : n * partial.(n - 1) }
+      }
+    )
 
-  fx.(14)
+  fx.(1)
 
 }.()
 
-# >> 87178291200
+# >> 1
